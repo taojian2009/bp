f3e4dd5f56ce398894c710ee340d924efec0240f
jiantao01 <jian.tao@afterpay.com>
2021-09-12 20:51:48 +0800

    update

---
+++ practice/src/Prac.java
@@ -0,0 +1,11 @@
+public class Prac {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 12;
+        char c = 'b';
+        System.out.println(b + c);
+        int maxValue = Math.max(a, b);
+        System.out.printf("max value is %s", maxValue);
+    }
+}
+
