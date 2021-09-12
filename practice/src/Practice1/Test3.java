d0e4fe7f45ceb7784215df22ef0146292540af22
jiantao01 <jian.tao@afterpay.com>
2021-09-12 20:40:48 +0800

    test3

---
+++ practice/src/Practice1/Test3.java
@@ -0,0 +1,46 @@
+/*
+请根据下列描述，选用合适的数据类型定义变量，并输出变量的值。
+
+1、班级里有100个学员。
+
+2、某商品价格55.55元。
+
+3、地球已经诞生超过46亿年。
+
+4、“5是偶数”这句话的描述是假的。
+
+输出结果如下：
+
+
+100
+55.55
+4600000000
+false
+ */
+
+package Practice1;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+        //1、班级里有100个学员。
+        int studentCount = 100;
+        //2、某商品价格55.55元。
+        float goodPrice = 55.55f;
+        //3、地球已经诞生超过46亿年。
+        long age = 4600000000L;
+        //4、“5是偶数”这句话的描述是假的。
+        boolean isOdd = false;
+        System.out.println(studentCount);
+        System.out.println(goodPrice);
+        System.out.println(age);
+        System.out.println(isOdd);
+    }
+
+    //
+
+    //
+
+    //
+
+}
