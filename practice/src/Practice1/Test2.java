11787e60113bdc7cf68924c99e1c4b576fb17400
jiantao01 <jian.tao@afterpay.com>
2021-09-12 20:20:03 +0800

    test2

---
+++ practice/src/Practice1/Test2.java
@@ -0,0 +1,24 @@
+package Practice1;
+
+public class Test2 {
+    public static void main(String[] args) {
+        byte a = 1;
+        short b = 2;
+        int c = 2;
+        long d = 3;
+        boolean boo = false;
+        String str = "hello";
+        float flo = 12.4324f;
+        double dou = 123.23d;
+        System.out.printf(
+                "1:%s\n" +
+                        "2:%s\n" +
+                        "3:%s\n" +
+                        "4:%s\n" +
+                        "5:%s\n" +
+                        "6:%s\n" +
+                        "7:%s\n" +
+                        "8:%s\n"
+                , a, b, c, d, boo, str, flo, dou);
+    }
+}
